a35f83dd2721c2d717898f6eb703c2d7481b4c35
chris ogee <chrisbisi44@gmail.com>
2023-06-21 07:43:56 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-include "main.h"
-
+#include "main.h"
 /**
- * main -entry point made by chris,
- *
- * Return: Always(0)
- */
-
+* main - prints the string "_putchar" from a character array.
+* Return: 0 on success.
+*/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+
 }
